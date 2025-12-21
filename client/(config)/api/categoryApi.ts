5082a01441424cd5de526e0b5c5c3d970fc816d7
Shahzaib Mirza <szb8480@gmail.com>
2025-12-22 00:22:25 +0500

    update: refactor file structure and added products api in rtk (#9)
    * refactor: integrate RTK Query into dashboard
    
    * update: refactor file structure and added products api in rtk
    
    ---------
    
    Co-authored-by: szbr1 <logintowrongR1!>

---
+++ client/(config)/api/categoryApi.ts
@@ -0,0 +1,18 @@
+import {api} from "./api";
+
+
+const productApi = api.injectEndpoints({
+     endpoints: (builder) => ({
+     
+        allCategoriesCount : builder.query({
+            query: () => 'api/category/totalCategories',
+            providesTags: ['Category']
+        }),
+       
+    }),
+    overrideExisting: false
+})
+
+
+export const {useAllCategoriesCountQuery} = productApi
+
