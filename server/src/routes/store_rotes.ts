cdce8ec992155957efd2f42a3ea9ca5f1dad13de
Shahzaib Mirza <szb8480@gmail.com>
2025-12-21 23:33:56 +0500

    feat: added cloudinary for photos upload, some endpoints update (#8)
    * update: new users and stores API endpoints
    
    * customers
    
    * feat: cloudinary get store products and update product endpoints
    
    ---------
    
    Co-authored-by: szbr1 <logintowrongR1!>

--- server/src/routes/store_rotes.ts
+++ server/src/routes/store_rotes.ts
@@ -1,5 +1,5 @@
 
-import { createStore, deleteStore, storeOrders, allCustomers, totalSales, totalStores, updateStore } from '@/controllers/store-controllers';
+import { createStore, deleteStore, storeOrders, allCustomers, totalSales, totalStores, updateStore, lastweekSales, lastyearSales } from '@/controllers/store-controllers';
 import express from 'express';
 import type { Router } from 'express';
 
@@ -12,5 +12,6 @@ route.get('/totalStores', totalStores);
 route.get('/storeOrdes', storeOrders)
 route.patch('/totalSales', totalSales);
 route.patch("/totalClients", allCustomers)
-
+route.patch('/lastweekSales', lastweekSales)
+route.patch('/lastyearSales', lastyearSales)
 export default route;
