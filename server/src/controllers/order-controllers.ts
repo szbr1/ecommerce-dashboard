fe59e98194bc05fb7a5599216e86a4152262c7c3
szbr1 <logintowrongR1!>
2025-12-29 11:06:26 +0500

    feat: recent orders and sales by time api created

--- server/src/controllers/order-controllers.ts
+++ server/src/controllers/order-controllers.ts
@@ -144,57 +144,7 @@ export const getOrders = async (req: Request, res: Response) => {
   }
 };
 
-export const getCompletedOrdersCount = async (req: Request, res: Response) => {
-  try {
-    const count = await prisma.order.count({
-      where: {
-        id: 1, // Todo
-        orderItems: {
-          some: {
-            order: {
-              deliveryStatus: 'COMPLETED',
-            },
-          },
-        },
-      },
-    });
 
-    return res.status(200).json({
-      totalOrders: count,
-    });
-  } catch (error) {
-    console.error('TOTAL ORDERS ERROR:', error);
-    return res.status(500).json({
-      message: 'Failed to count orders',
-    });
-  }
-};
-
-export const getFailedOrdersCount = async (req: Request, res: Response) => {
-  try {
-    const count = await prisma.order.count({
-      where: {
-        id: 1, // Todo
-        orderItems: {
-          some: {
-            order: {
-              deliveryStatus: 'CANCELLED',
-            },
-          },
-        },
-      },
-    });
-
-    return res.status(200).json({
-      totalOrders: count,
-    });
-  } catch (error) {
-    console.error('TOTAL ORDERS ERROR:', error);
-    return res.status(500).json({
-      message: 'Failed to count orders',
-    });
-  }
-};
 
 export const getOrdersPageCounts = async(req: Request, res: Response) => {
   try {
@@ -258,5 +208,113 @@ export const getOrdersPageCounts = async(req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     return res.status(500).json('failed while fetching order page counts.');
+  }
+};
+
+export const getRecentOrders = async (req: Request, res: Response) => {
+  try {
+    const result = await prisma.order.findMany({
+      where: {
+        orderItems: {
+          some: {
+            storeId: 1, // Todo
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc"
+      },
+      take: 5,
+      select: {
+        id: true,
+        amount: true,
+        user: {
+          select: {
+            name: true,
+            email: true
+          }
+        }
+      }
+    });
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json('Failed while getting orders');
+  }
+};
+
+export const getSalesByYear = async (req: Request, res: Response) => {
+  try {
+    const currentYear = new Date().getFullYear();
+
+    const result = await Promise.all(
+      Array.from({ length: 5 }).map(async (_, i) => {
+        const year = currentYear - i;
+
+        const start = new Date(year, 0, 1);
+        const end = new Date(year + 1, 0, 1);
+
+        const sales = await prisma.order.aggregate({
+          where: {
+            createdAt: {
+              gte: start,
+              lt: end,
+            },
+            paymentStatus: "PAID",
+          },
+          _sum: {
+            amount: true,
+          },
+        });
+
+        return {
+          year,
+          totalAmount: sales._sum.amount ?? 0,
+        };
+      })
+    );
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Failed to fetch yearly sales" });
+  }
+};
+
+export const getSalesByMonth = async (req: Request, res: Response) => {
+  try {
+    const now = new Date()
+
+    const result = await Promise.all(
+      Array.from({ length: 12 }).map(async (_, i) => {
+
+        const start = new Date(now.getFullYear(),now.getMonth()-i , 1);
+        const end = new Date(now.getFullYear(), now.getMonth()-i+1,1);
+
+        const sales = await prisma.order.aggregate({
+          where: {
+            createdAt: {
+              gte: start,
+              lt: end,
+            },
+            paymentStatus: "PAID",
+          },
+          _sum: {
+            amount: true,
+          },
+        });
+
+        return {
+          month: start.toLocaleDateString("default", {month: "short"}),
+          totalAmount: sales._sum.amount ?? 0,
+        };
+      })
+    );
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Failed to fetch yearly sales" });
   }
 };
--- server/src/routes/order_routes.ts
+++ server/src/routes/order_routes.ts
@@ -1,5 +1,5 @@
 
-import { createOrder, deleteOrder, getOrders,  getOrder, update, getOrdersPageCounts,  } from "../controllers/order-controllers"
+import { createOrder, deleteOrder, getOrders,  getOrder, update, getOrdersPageCounts, getSalesByYear, getSalesByMonth, getRecentOrders,  } from "../controllers/order-controllers"
 import type { Router } from "express";
 import express from "express";
 
@@ -12,4 +12,7 @@ route.patch('/updateOrder', update)
 route.get('/getOrder', getOrder)
 route.get('/getOrders', getOrders)
 route.get('/getOrdersPageCounts', getOrdersPageCounts)
+route.get('/getRecentOrders', getRecentOrders)
+route.get('/getSalesByYear', getSalesByYear)
+route.get('/getSalesByMonth', getSalesByMonth)
 export default route
